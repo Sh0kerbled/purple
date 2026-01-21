08c84b2ee10e7c49160cbf5fddb9bc8e67dd29ef
Dauren <d.k.nurgaziev@gmail.com>
2026-01-21 10:45:54 +0500

    features contariner

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -1,9 +1,71 @@
 <template>
   <div
-    class="bg-linear-to-r from-[#190D2E] to-[#020103] mt-20 w-300 grid justify-center"
+    class="grid mx-auto px-12.5 py-19 bg-linear-to-r from-[#190D2E] to-[#020103] mt-20 w-300 rounded-2xl"
   >
-    <div class="text-white text-[56px] w-88.5 pt-19">
+    <div class="text-white text-[56px]/[65px] w-88.5">
       Elevate your SEO efforts.
+    </div>
+    <div class="flex flex-wrap gap-15">
+      <div class="w-80.5">
+        <div class="text-[16px] text-white pt-10 w-64.75">
+          User-friendly dashboard
+          <p class="text-white/70 pt-2.5">
+            Perform complex SEO audits and optimizations with a single click.
+          </p>
+        </div>
+        <div class="text-[16px] text-white pt-10 w-64.75">
+          Content evaluation
+          <p class="text-white/70 pt-2.5">
+            Simple corrections for immediate improvemens.
+          </p>
+        </div>
+        <div class="text-[16px] text-white pt-10 w-64.75">
+          Link Optimization Wizard
+          <p class="text-white/70 pt-2.5">
+            Guides you through the process of creating and managing links.
+          </p>
+        </div>
+      </div>
+      <div class="w-80.5">
+        <div class="text-[16px] text-white pt-10 w-64.75">
+          User-friendly dashboard
+          <p class="text-white/70 pt-2.5">
+            Perform complex SEO audits and optimizations with a single click.
+          </p>
+        </div>
+        <div class="text-[16px] text-white pt-10 w-64.75">
+          Content evaluation
+          <p class="text-white/70 pt-2.5">
+            Simple corrections for immediate improvemens.
+          </p>
+        </div>
+        <div class="text-[16px] text-white pt-10 w-64.75">
+          Link Optimization Wizard
+          <p class="text-white/70 pt-2.5">
+            Guides you through the process of creating and managing links.
+          </p>
+        </div>
+      </div>
+      <div class="w-80.5">
+        <div class="text-[16px] text-white pt-10 w-64.75">
+          User-friendly dashboard
+          <p class="text-white/70 pt-2.5">
+            Perform complex SEO audits and optimizations with a single click.
+          </p>
+        </div>
+        <div class="text-[16px] text-white pt-10 w-64.75">
+          Content evaluation
+          <p class="text-white/70 pt-2.5">
+            Simple corrections for immediate improvemens.
+          </p>
+        </div>
+        <div class="text-[16px] text-white pt-10 w-64.75">
+          Link Optimization Wizard
+          <p class="text-white/70 pt-2.5">
+            Guides you through the process of creating and managing links.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
